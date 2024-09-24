f98af774c14cb354db88b0bd018ee9c2fa3f88eb
Tanishq Pandey <tanishqvatsa26@gmail.com>
2024-09-24 15:07:30 +0530

    new layout for experience and project section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,144 +1,128 @@
 import React from 'react';
-import {BsPatchCheckFill} from "react-icons/bs";
-import {TbBadgesFilled} from "react-icons/tb";
+import { BsPatchCheckFill } from "react-icons/bs";
+import { TbBadgesFilled } from "react-icons/tb";
 import "./ExperienceStyles.css";
 
 function Experience() {
 
-  
   function scrollHandler(targetId) {
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
-        targetSection.scrollIntoView({ behavior: "smooth" });
+      targetSection.scrollIntoView({ behavior: "smooth" });
     }
-}
-
+  }
 
   return (
-   
     <>
-    
-    <section id="experience">
-      <p class="section__text__p1">Explore My</p>
-      <h1 class="title">Experience</h1>
-      <div class="experience-details-container">
-        <div class="about-containers">
-          <div class="details-container">
-            <h2 class="experience-sub-title">Frontend Development</h2>
-            <div class="article-container">
+      <section id="experience">
+        <p className="section__text__p1">Explore My</p>
+        <h1 className="title">Experience</h1>
+        
+        <div className="experience-details-container">
+          <div className="about-containers">
+           
+            <div className="details-container tech-stack-card">
+              <h2 className="experience-sub-title">Tech Stack & Skills</h2>
+              <div className="article-container">
 
-            <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>React.js</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+               
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Next.js</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>Next.js</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>TypeScript</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>JavaScript</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>React.js</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>TypeScript</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>React Native</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                            <div>
-                  <h3>HTML</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Node.js</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>CSS</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Express.js</h3>
+                  </div>
+                </article>
 
-          </div>
-          </div>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Java</h3>
+                  </div>
+                </article>
 
-          <div class="details-container">
-            <h2 class="experience-sub-title">Backend Development</h2>
-            <div class="article-container">
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>JavaScript</h3>
+                  </div>
+                </article>
 
-            <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>Node.js</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>MongoDB</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>Express.js</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>SQL</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>Firebase</h3>
-                  <p>Experienced</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Tailwind CSS</h3>
+                  </div>
+                </article>
 
-              <article>
-               <BsPatchCheckFill 
-               size={30}/>
-                <div>
-                  <h3>Git</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
+                <article>
+                  <BsPatchCheckFill size={30} />
+                  <div>
+                    <h3>Git</h3>
+                  </div>
+                </article>
 
+              </div>
             </div>
           </div>
-
-          
         </div>
-      </div>
-      <TbBadgesFilled
-    className="icon arrow"
-    size={30}  // adjust this for the desired size
-    onClick={() => scrollHandler('projects')}
-/>
-    </section>
-    
+
+        <TbBadgesFilled
+          className="icon arrow"
+          size={30}
+          onClick={() => scrollHandler('projects')}
+        />
+      </section>
     </>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cotlog from "../material/Cotlog.png";
 import Tripper from "../material/Tripper.png";
-import {TbBadgesFilled} from "react-icons/tb";
+import { TbBadgesFilled } from "react-icons/tb";
 import "./ProjectsStyles.css";
 
 function Projects() {
@@ -9,112 +9,76 @@ function Projects() {
   function scrollHandler(targetId) {
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
-        targetSection.scrollIntoView({ behavior: "smooth" });
+      targetSection.scrollIntoView({ behavior: "smooth" });
     }
-}
-
+  }
 
   return (
-    
     <>
-    
-    <section id="projects">
-      <p class="section__text__p1">Browse My Recent</p>
-      <h1 class="title">Projects</h1>
-      <div class="experience-details-container">
-        <div class="about-containers">
-
-          {/* <div class="details-container color-container">
-            <div class="article-container">
-              <img
-                src="./assets/project-1.png"
-                alt="Project 1"
-                class="project-img"
-              />
+      <section id="projects">
+        <p className="section__text__p1">Browse My Recent</p>
+        <h1 className="title">Projects</h1>
+        <div className="projects-container">
+          
+          <div className="project-card">
+            <div className="project-img-container">
+              <img src={Tripper} alt="Project 2" className="project-img" />
+              <div className="btn-container">
+                <button
+                  className="btn btn-color-2 project-btn"
+                  onClick={() => window.open("https://github.com/tanishqpandey26/Tripper")}
+                >
+                  Github
+                </button>
+                <button
+                  className="btn btn-color-2 project-btn"
+                  onClick={() => window.open("https://tripper-ui.vercel.app/")}
+                >
+                  Live Demo
+                </button>
+              </div>
             </div>
-            <h2 class="experience-sub-title project-title">Project One</h2>
-            <div class="btn-container">
-              <button
-                class="btn btn-color-2 project-btn"
-                onclick="location.href='https://github.com/'"
-              >
-                Github
-              </button>
-              <button
-                class="btn btn-color-2 project-btn"
-                onclick="location.href='https://github.com/'"
-              >
-                Live Demo
-              </button>
+            <div className="project-description">
+              <h2 className="project-title">Project-One : Tripper</h2>
+              <p className="project-text"> Tripper is a comprehensive trip planning platform designed to streamline the travel experience for both casual tourists and adventure seekers. With Tripper, users can effortlessly create, manage, and share personalized travel itineraries, track expenses, and share their experiences with fellow travelers. The platform aims to enhance the travel journey by providing all-in-one trip management tools, fostering a community of travel enthusiasts, and ensuring seamless collaboration between travel companions.</p>
             </div>
-          </div> */}
+          </div>
 
           
-          <div class="details-container color-container">
-            <div class="article-container">
-              <img
-                src={Tripper}
-                alt="Project 2"
-                class="project-img"
-              />
+          <div className="project-card">
+            <div className="project-img-container">
+              <img src={Cotlog} alt="Project 3" className="project-img" />
+              <div className="btn-container">
+                <button
+                  className="btn btn-color-2 project-btn"
+                  onClick={() => window.open("https://github.com/tanishqpandey26/Cotlog-social_blog_website")}
+                >
+                  Github
+                </button>
+                <button
+                  className="btn btn-color-2 project-btn"
+                  onClick={() => window.open("https://cotlog-social-blog-website.vercel.app/")}
+                >
+                  Live Demo
+                </button>
+              </div>
             </div>
-            <h2 class="experience-sub-title project-title">Project-One</h2>
-            <div class="btn-container">
-              <button
-                class="btn btn-color-2 project-btn"
-                onClick={()=>window.open("https://github.com/tanishqpandey26/Tripper")}
-              >
-                Github
-              </button>
-              <button
-                class="btn btn-color-2 project-btn"
-               onClick={()=>window.open("https://tripper-ui.vercel.app/")}
-              >
-                Live Demo
-              </button>
-            </div>
-          </div>
-
-          <div class="details-container color-container">
-            <div class="article-container">
-              <img
-                src={Cotlog}
-                alt="Project 3"
-                class="project-img"
-              />
-            </div>
-            <h2 class="experience-sub-title project-title">Project-Two</h2>
-            <div class="btn-container">
-              <button
-                class="btn btn-color-2 project-btn"
-                onClick={()=>window.open("https://github.com/tanishqpandey26/Cotlog-social_blog_website")}
-              >
-                Github
-              </button>
-              <button
-                class="btn btn-color-2 project-btn"
-                onClick={()=>window.open("https://cotlog-social-blog-website.vercel.app/")}
-              >
-                Live Demo
-              </button>
+            <div className="project-description">
+              <h2 className="project-title">Project-Two : Cotlog</h2>
+              <p className="project-text">CotLog is a social blogging platform designed to empower students and education seekers by providing authentic, firsthand reviews of colleges, universities, and academic courses. Whether you're a high school graduate planning your undergraduate studies, considering postgraduate opportunities, or exploring new fields of learning, CotLog serves as a valuable resource to guide you in making informed academic decisions. Our platform fosters a vibrant community where users can connect, share personal experiences, and exchange insights to help others navigate the complex world of higher education.</p>
             </div>
           </div>
-
-
 
         </div>
-      </div>
 
-      <TbBadgesFilled
-      className="icon arrow"
-      size={30}  
-      onClick={() => scrollHandler('contact')}
-      />
-
-    </section>
-    
+        <TbBadgesFilled
+          className="icon arrow"
+          size={30}
+          onClick={() => scrollHandler('contact')}
+        />
+      </section>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
