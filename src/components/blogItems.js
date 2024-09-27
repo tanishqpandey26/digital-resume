c430d68fb626b8de82a35be9d4b9f63e5c50092e
Tanishq Pandey <tanishqvatsa26@gmail.com>
2024-09-27 11:05:34 +0530

    new blog added

--- src/components/blogItems.js
+++ src/components/blogItems.js
@@ -1,6 +1,12 @@
 export const blogItems = [
 
   {
+    title: "Second blog on podcast summary",
+    content: `The second blog in this series, 'Using Your Nervous System to Enhance Your Immune System' covers the following: <br/>  1. Practice nasal breathing and avoid touching your eyes. Consume 2-4 servings of fermented foods daily. <br/> 2. Elevate your heels by 12 degrees to support a better immune response. Try NSDR (Non-Sleep Deep Rest) with your feet elevated. <br/> 3. A 15-minute sauna session can enhance the immune system by increasing white blood cells. Aim for humidity levels of ±15%, with a cool-off period of 5 to 10 minutes before returning to the sauna. <br/> 4. Eating foods rich in serotonin can help combat infections. <br/> 5. Perform a breathing exercise: 25-30 inhales and exhales, followed by an exhale and hold period. Complete 3 sets of this exercise. <br/> 6. Maintaining a healthy mindset can aid in preventing and fighting sickness. Higher dopamine levels may also be beneficial. <br/> 7. Spirulina (2g) can help alleviate rhinitis. <br/> 8. Acupuncture may also be helpful but is highly individualized. `,
+    date: "September 27, 2024",
+  },
+
+  {
     title: "First blog on podcast summary",
     content: "The first blog in this series on : 'Optimal Protocols for Studying & Learning' <br/>  1. Periodic testing is crucial. <br/> 2. The strain you feel trying to learn is good. <br/> 3. Perception exercise is good, teaching what you have learned to others helps you remember things better, make schedules for learning. <br/> 4. Reading and thinking about what you have read is better than re-reading same material multiple times. <br/> 5. Testing is the greatest learning tool, frequency of testing increases learning efficiency. <br/> 6. The best time to self test is just after being exposed to the new material, followed by a test after a period. <br/> 7. Self test should be open ended and not mcqs. <br/> 8.  Any kind of story with emotional emphasis is more likely to be readily commited to memory, reason - neuromodulators. <br/> 9. Cold exposure, caffeine, good sleep also do contribute to effective learning. <br/> 10. Interleaving information also does help. ",
     date: "September 18, 2024",
